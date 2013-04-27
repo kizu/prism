9fef47f062ad1db0efa020eafc5d5730ec03ae11
kizu <kizmarh@gmail.com>
2013-04-27 23:31:56 +0400

    First draft. Known issues: problems with markup, sometimes clike/coffeescript win against others

--- plugins/autodetect/prism-autodetect.js
+++ plugins/autodetect/prism-autodetect.js
@@ -4,6 +4,37 @@ if (!self.Prism) {
 	return;
 }
 
-console.log("Here would be something nice")
+function detectLanguage(element) {
+	var result = 'none';
+	var resultScore = 0;
+
+	for (key in Prism.languages) {
+		var tokensCount = 0;
+		var unparsedCount = 0;
+		if (typeof Prism.languages[key] === 'object') {
+			tokens = Prism.tokenize(element.textContent,Prism.languages[key]);
+			for (var i=0, token; token = tokens[i++];) {
+				if (typeof token === 'string') {
+					unparsedCount++;
+				} else{
+					tokensCount++;
+				}
+			}
+			if (tokensCount - unparsedCount > resultScore) {
+				result = key;
+				resultScore = tokensCount - unparsedCount;
+			}
+		}
+	}
+	return result;
+}
+
+var elements = document.querySelectorAll('code:not([class*="language-"])');
+
+for (var i=0, element; element = elements[i++];) {
+	language = detectLanguage(element);
+	element.className = element.className + ' language-' + language;
+	Prism.highlightElement(element, false);
+}
 
 })();
